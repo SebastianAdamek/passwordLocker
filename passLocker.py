2959b829704b8ab747a0a85258edbdf856a2ceb7
sebadamek <sebadamek@wp.pl>
2018-07-18 14:13:07 +0200

    new file

---
+++ passLocker.py
@@ -0,0 +1,9 @@
+#! python3
+# -*- coding: UTF-8 -*-
+# passLocker.py  - An insecure password locker program.
+
+
+PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+             'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+             'luggage': '12345'}
+
